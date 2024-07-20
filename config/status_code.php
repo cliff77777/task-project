9f0b0101eca4791d8445720d98f0f0514d4ba565
cliff77777 <s19910507@gmail.com>
2024-07-20 23:24:30 +0800

    add status_code config file

---
+++ config/status_code.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'success' => [
+        'email_verified' => 'Your email is already verified.',
+        'verification_link_sent' => 'Verification link sent!',
+    ],
+    'error' => [
+        'email_not_verified' => 'Your email is not verified.',
+        'invalid_credentials' => 'Invalid login credentials.',
+    ],
+    'info' => [
+        'welcome' => 'Welcome to our application!',
+    ],
+];
